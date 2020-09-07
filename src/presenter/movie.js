ee5af35310bad9dc614fa74ed83592f62520507b
ipolekhin <ipolekhin@yadnex.ru>
2020-09-07 15:19:41 +0300

    5.2 Запустим сортировку

--- src/presenter/movie.js
+++ src/presenter/movie.js
@@ -1,3 +1,4 @@
+import SortView from "../view/sort.js";
 import FilmsBlocksContainerView from "../view/films-blocks-container.js";
 import FilmsContainerView from "../view/films-container.js";
 import FilmsListView from "../view/films-list.js";
@@ -7,16 +8,20 @@ import FilmItemView from "../view/film-item.js";
 import FilmDetailsPopupView from "../view/film-details-popup.js";
 import {remove, render, RenderPosition} from "../utils/render.js";
 import {generateComments} from "../mock/comment.js";
-import {EXTRA_BLOCK_NAMES, Keys} from "../const.js";
+import {ExtraBlockNames, EXTRA_BLOCK_NAMES, Keys, SortType} from "../const.js";
 
 const FILMS_COUNT_PER_STEP = 5;
-const EXTRA_FILMS_CARD_COUNT = 2;
 
 export default class Movie {
   constructor(movieContainer, footerElement) {
+    this._siteMainElement = null;
+    this._currentSortType = SortType.DEFAULT;
+    this._sortComponent = new SortView();
+    this._handleSortTypeChange = this._handleSortTypeChange.bind(this);
     this._movieContainer = movieContainer;
     this._footerElement = footerElement;
     this._renderedFilmCount = FILMS_COUNT_PER_STEP;
+    this._filmsBlocksContainerComponent = new FilmsBlocksContainerView();
     this._filmsBlocksContainerComponent = new FilmsBlocksContainerView();
     this._filmsContainerComponent = new FilmsContainerView();
     this._filmsListComponent = new FilmsListView();
@@ -25,18 +30,53 @@ export default class Movie {
     this._noFilmsComponent = new EmptyFilmsView();
   }
 
-  init(listFilms) {
+  init(listFilms, siteMainElement) {
     this._listFilms = listFilms.slice();
+    this._sourcedListFilms = listFilms.slice();
+    this._siteMainElement = siteMainElement;
+    this._renderSort();
     // Метод для инициализации (начала работы) модуля,
     // малая часть текущей функции renderList в main.js
     render(this._movieContainer, this._filmsBlocksContainerComponent);
-
     this._renderMovieListContainer();
   }
 
-  // todo
+  _sortTasks(sortType) {
+    // - Сортируем фильмы
+    // 2. Этот исходный массив задач необходим,
+    // потому что для сортировки мы будем мутировать
+    // массив в свойстве _listFilms
+    switch (sortType) {
+      case SortType.DATE:
+        this._listFilms.sort((a, b) => a.releaseDate - b.releaseDate);
+        break;
+      case SortType.RATING:
+        this._listFilms.sort((a, b) => b.rating * 10 - a.rating * 10);
+        break;
+      default:
+        // 3. А когда пользователь захочет "вернуть всё, как было",
+        // мы просто запишем в _listFilms исходный массив
+        this._listFilms = this._sourcedListFilms.slice();
+    }
+
+    this._currentSortType = sortType;
+  }
+
+  _handleSortTypeChange(sortType) {
+    if (this._currentSortType === sortType) {
+      return;
+    }
+    this._sortTasks(sortType);
+    // - Очищаем фильмы
+    this._clearMovieList();
+    // - Рендерим фильмы заново
+    this._renderMovieList();
+  }
+
   _renderSort() {
     // Метод для рендеринга сортировки
+    render(this._siteMainElement, this._sortComponent);
+    this._sortComponent.setSortTypeChangeHandler(this._handleSortTypeChange);
   }
 
   _renderFilm(film, filmsListComponent = this._filmsListComponent) {
@@ -104,21 +144,40 @@ export default class Movie {
   }
 
   _renderExtraBlock() {
+    let sortedMovies = [];
+    const showingMovies = this._listFilms.slice();
+
     EXTRA_BLOCK_NAMES.forEach((title) => {
       const filmsExtraContainerComponent = new FilmsContainerView(`--extra`, title);
       const extraFilmsListComponent = new FilmsListView();
       render(this._filmsBlocksContainerComponent, filmsExtraContainerComponent);
       render(filmsExtraContainerComponent, extraFilmsListComponent);
-      for (let i = 0; i < EXTRA_FILMS_CARD_COUNT; i++) {
-        this._renderFilm(this._listFilms[i], extraFilmsListComponent);
+
+      switch (title) {
+        case ExtraBlockNames.TOP_RATED:
+          sortedMovies = showingMovies.sort((a, b) => b.rating * 10 - a.rating * 10);
+
+          sortedMovies.slice(0, 2).forEach((film) => {
+            this._renderFilm(film, extraFilmsListComponent);
+          });
+          break;
+        case ExtraBlockNames.MOST_COMMENTED:
+          sortedMovies = showingMovies.sort((a, b) => b.commentsCount - a.commentsCount);
+
+          sortedMovies.slice(0, 2).forEach((film) => {
+            this._renderFilm(film, extraFilmsListComponent);
+          });
+          break;
       }
     });
   }
 
-  _renderMovieList() {
-    render(this._filmsBlocksContainerComponent, this._filmsContainerComponent);
-    render(this._filmsContainerComponent, this._filmsListComponent);
+  _clearMovieList() {
+    this._filmsListComponent.getElement().innerHTML = ``;
+    this._renderedFilmCount = FILMS_COUNT_PER_STEP;
+  }
 
+  _renderMovieList() {
     this._renderFilms(0, Math.min(this._listFilms.length, FILMS_COUNT_PER_STEP));
 
     if (this._listFilms.length > FILMS_COUNT_PER_STEP) {
@@ -135,6 +194,8 @@ export default class Movie {
       return;
     }
 
+    render(this._filmsBlocksContainerComponent, this._filmsContainerComponent);
+    render(this._filmsContainerComponent, this._filmsListComponent);
     this._renderMovieList();
     this._renderExtraBlock();
   }
